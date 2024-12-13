5f25daa58de95860b972b3dd49b9a382457705c4
Kerri Salciccioli <kerri@salciccio.li>
2024-12-13 01:57:45 -0500

    Working code! yay

--- index.js
+++ index.js
@@ -1,13 +1,63 @@
-// TODO: Include packages needed for this application
+import { input, select } from '@inquirer/prompts';
+import { writeFile } from 'fs/promises';
+import { generateMarkdown, LICENSES } from './utils/generateMarkdown.js';
 
-// TODO: Create an array of questions for user input
-const questions = [];
+/**
+ * Write the given data to a file with the given filename.
+ * @param {string} fileName 
+ * @param {string} data 
+ */
+function writeToFile(fileName, data) {
+    writeFile(fileName, data, 'utf8');
+}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+/**
+ * Ensures a string isn't empty.
+ * @param {string} answer 
+ * @returns {boolean}
+ */
+function validateNotEmpty(answer) {
+    return answer !== '';
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+/**
+ * Initialize the app and get user input, then write the generated markdown to README.md.
+ * This function must be async due to using `await` for the prompts.
+ */
+async function init() {
+    // Turn the LICENSES array into a choices array that `@inquirer/select` will understand.
+    const licenseChoices = LICENSES.map((license) => {
+        return {
+            name: license.name,
+            value: license.keyword,
+            description: license.url
+        }
+    });
+
+    const data = {
+        title: await input({ message: 'Enter your project title: ', validate: validateNotEmpty }),
+        desc: await input({ message: 'Enter a description for your project: ', validate: validateNotEmpty }),
+        install: await input({ message: 'Enter installation instructions for your project: ', validate: validateNotEmpty }),
+        usage: await input({ message: 'Enter usage information for your project: ', validate: validateNotEmpty }),
+        contrib: await input({ message: 'Enter contribution guidelines: ', validate: validateNotEmpty }),
+        test: await input({ message: 'Enter test instructions for your project: ', validate: validateNotEmpty }),
+        license: await select({
+            message: 'Choose a license for your project:',
+            choices: licenseChoices
+        }),
+        githubUser: await input({ message: 'Enter your GitHub username: ', validate: validateNotEmpty }),
+        email: await input({
+            message: 'Enter your email address: ',
+            // Make sure the email *is* an email address. More or less.
+            // I'm not using something to fully test for RFC 5322 compliance in a homework assignment
+            validate: (email) => /\S+@\S+\.\S+/.test(email)
+        })
+    }
+
+    const markdown = generateMarkdown(data);
+
+    writeToFile('README.md', markdown);
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,144 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// small subset of licenses from
+// https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/licensing-a-repository#searching-github-by-license-type'
+// keys:
+// - name: the full name of the license, used in the license section of the README and the license choosing prompt
+// - keyword: the keyword to display in the badge (pulled from the github page above)
+// - url: the URL of the license text, used in the license choosing prompt and the license section in the README
+const LICENSES = [
+  {
+    name: 'Apache 2.0',
+    keyword: 'Apache-2.0',
+    url: 'https://www.apache.org/licenses/LICENSE-2.0'
+  },
+  {
+    name: 'ISC',
+    keyword: 'ISC',
+    url: 'https://opensource.org/license/isc-license-txt'
+  },
+  {
+    name: 'GNU General Public License v3.0',
+    keyword: 'GPL-3.0',
+    url: 'https://www.gnu.org/licenses/gpl-3.0.en.html#license-text'
+  },
+  {
+    name: 'MIT License',
+    keyword: 'MIT',
+    url: 'https://opensource.org/licenses/MIT'
+  },
+  {
+    name: 'Mozilla Public License 2.0',
+    keyword: 'MPL-2.0',
+    url: 'https://www.mozilla.org/en-US/MPL/2.0/'
+  },
+  {
+    name: 'Unlicense',
+    keyword: 'Unlicense',
+    url: 'https://unlicense.org/'
+  },
+  {
+    name: 'No license',
+    keyword: '',
+    url: ''
+  }
+];
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+/**
+ * hacky-hack because inquirer's `select()` only returns a string (the keyword in this case) and
+ * I need to get the URL and full name out of the LICENCES array (declared above)
+ * @param {string} licenseKey 
+ * @returns object
+ */
+function getLicenseFromKey(licenseKey) {
+  return LICENSES.filter(license => license.keyword === licenseKey)[0];
 }
 
-export default generateMarkdown;
+/**
+ * Render a markdown image link for the license, using shields.io
+ * @param {string} licenseKey 
+ * @returns string
+ */
+function renderLicenseBadge(licenseKey) {
+  if (!licenseKey) {
+    return '';
+  }
+
+  const license = getLicenseFromKey(licenseKey);
+  // parse the license keyword so it appears correctly in the badge from shields.io
+  // see https://shields.io/badges
+  const badgeText = licenseKey.replaceAll('_', '__')
+    .replaceAll(' ', '_')
+    .replaceAll('-', '--');
+
+  return `![${license.name}](https://img.shields.io/badge/License-${badgeText}-red?style=plastic)`;
+}
+
+/**
+ * Return a markdown link for the provided license's full text
+ * @param {sring} licenseKey 
+ * @returns string
+ */
+function renderLicenseLink(licenseKey) {
+  if (!licenseKey) {
+    return '';
+  }
+
+  const license = getLicenseFromKey(licenseKey);
+
+  return `[${license.name}](${license.url})`;
+}
+
+/**
+ * Return the markdown for the `License` section in the README
+ * @param {string} licenseKey 
+ * @returns string
+ */
+function renderLicenseSection(licenseKey) {
+  if (!licenseKey) {
+    return '';
+  }
+
+  return `## Lincense\nThis license uses the ${renderLicenseLink(licenseKey)} license.`;
+}
+
+/**
+ * Generate the full markdown for the README
+ * @param {object} data 
+ * @returns string
+ */
+function generateMarkdown(data) {
+  return `# ${data.title}
+${renderLicenseBadge(data.license)}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [How to Contribute](#contributing)
+${data.license !== '' ? '- [License](#license)' : ''}
+- [Tests](#tests)
+- [Questions?](#questions)
+
+## Description
+${data.desc}
+
+## Installation
+${data.install}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contrib}
+
+${renderLicenseSection(data.license)}
+
+## Tests
+${data.test}
+
+## Questions
+You can find my GitHub profile [here](https://github.com/${data.githubUser}).
+ 
+You can contact me at [${data.email}](mailto:${data.email}) if you have any further questions.`;
+}
+
+export { LICENSES, generateMarkdown };
